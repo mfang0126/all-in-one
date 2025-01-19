7d023f97ca1c39fda23a9ffd8606642254c35a42
Ming Fang <mfang0126@gmail.com>
2025-01-19 16:17:37 +1100

    update config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   async rewrites() {
+    const apiUrl =
+      process.env.API_URL || (process.env.NODE_ENV === 'production' ? 'https://api-dev.pteyyds.com' : 'https://api-freedom.pteyyds.com');
+
     return [
       {
         source: '/api/:path*',
-        destination: 'https://api-freedom.pteyyds.com/api/:path*'
+        destination: `${apiUrl}/api/:path*`
       }
     ];
   }
